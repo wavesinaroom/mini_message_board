fea315b009501d69b0bcb0f3482d3adb3a5bc4ef
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-16 17:12:28 -0500

    [Feature] Message board initial messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  }, 
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Message Board', messages:messages});
 });
 
 module.exports = router;
